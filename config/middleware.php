337cba914d5b9b472a7ba20ec413b906960b7047
Jan-Paul Kleemans <jpkleemans@gmail.com>
2016-06-11 15:36:56 +0200

    Add sample middleware

--- config/middleware.php
+++ config/middleware.php
@@ -1,5 +1,18 @@
 <?php
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 return [
-    // Add your application middleware here
+    function (Request $request, Response $response, callable $next) {
+        $request = $request->withAttribute('request_time', time());
+
+        return $next($request, $response);
+    },
+    function (Request $request, Response $response, callable $next) {
+        $time = $request->getAttribute('request_time');
+        $response->getBody()->write("Hello World!<br><small>Requested at: $time</small>");
+
+        return $next($request, $response);
+    },
 ];
